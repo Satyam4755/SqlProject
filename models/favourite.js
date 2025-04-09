f165acb2ec1f645f132c7d561a4624a605697e8a
Satyam Raj <satyambpl.2000@gmail.com>
2025-04-09 21:05:55 +0530

    Update favourite.js

--- models/favourite.js
+++ models/favourite.js
@@ -1,36 +1,26 @@
+const db = require('../utils/dataBase');
 
-const fs=require('fs')
-const rootPath=require('../utils/rootPath')
-const path=require('path') 
-const FavfilePath=path.join(rootPath,'data','favourite.json')
+module.exports = class Favourite {
+    // constructor(homeId) {
+    //     this.homeId = homeId;
+    // }
 
-module.exports=class Favourite{
-    static addFavourite(homeId,callBack){
-        Favourite.getFavourite(favourites=>{
-            if(favourites.includes(homeId)){
-                callBack("already added")
-            }else{
-                favourites.push(homeId)
-                fs.writeFile(FavfilePath,JSON.stringify(favourites),callBack)
-            }
-        })
-    }
-    static getFavourite(callBack){
-        fs.readFile(FavfilePath,(err,data)=>{
-            callBack(!err?JSON.parse(data):[])
-        })
-    }
-    // to unfavourite the home
-    static unfavourite(id){
-        Favourite.getFavourite(homeIds=>{
-            // NOTE: isme sirf id hi rahegi to id ko hi hatana hai isiliye hum homeIds hi maan rhe hai
-            homeIds=homeIds.filter(homeId=>id!==homeId)
-            fs.writeFile(FavfilePath,JSON.stringify(homeIds),(error)=>{
-                if(error){
-                    console.log("error")
+    static addFavourite(homeId) {
+        return db.execute("SELECT homeId FROM favouriteHomes WHERE homeId = ?", [homeId])
+            .then(([rows]) => {
+                if (rows.length > 0) {
+                    return Promise.reject("already added");
+                } else {
+                    return db.execute("INSERT INTO favouriteHomes(homeId) VALUES (?)", [homeId]);
                 }
-            })
-        })
+            });
+    }
+
+    static getFavourite() {
+        return db.execute("SELECT * FROM favouriteHomes");
+    }
+
+    static unfavourite(id) {
+        return db.execute("DELETE FROM favouriteHomes WHERE homeId = ?", [id]);
     }
 }
-
